cf1ad9629aaec7a188b64022c74ae9baa9485111
PabloVives30 <48116412@est.ort.edu.ar>
2024-05-27 15:51:18 -0300

    login is ok

--- Assets/loginManager.cs
+++ Assets/loginManager.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class loginManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public InputField inputPassword;
+  
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Contra()
+    {
+        
+      if (inputPassword.text == "PabloVives")
+        {
+            Debug.Log("Access Granted");
+        }
+        else
+        {
+            Debug.Log("Access Deniend");
+        }
     }
 }
